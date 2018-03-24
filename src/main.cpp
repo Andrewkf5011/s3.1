4ddd156cb8db8100295bdb492ddb7c10f7235d84
AndrewBiscuit <andrewalford1@outlook.com>
2018-03-24 16:49:07 +0000

    Solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,18 @@
 #include <mbed.h>
+#include "components.h"
 
-int main() {
-    AnalogIn  left(A0);
-    AnalogIn right(A1);
-    while(1) {
-        printf(" Left dial %f \n",  left.read() );
+int main()
+{
+    AssignmentBoard board;
+
+    Potentiometer  left(board.SHIELD_LEFT_POTENTIOMETER);
+    Potentiometer right(board.SHIELD_RIGHT_POTENTIOMETER);
+
+    while(1)
+    {
+        printf("Left dial %f \n",  left.read() );
         printf("Right dial %f \n", right.read() );
+
         wait( 1.0 );
     }
 }
